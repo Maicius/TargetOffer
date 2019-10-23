fb950f5396f99afd6e0bf9fd7c41eb92c28f79b0
Maicius <maicius@outlook.com>
2019-10-23 12:33:45 +0800

    从左到右打印二叉树

---
+++ PrintFromTopToBottom.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution {
+public:
+    
+    vector<int> PrintFromTopToBottom(TreeNode* root) {
+        vector<int> res;
+        if (!root)
+            return res;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty()) {
+            res.push_back(q.front()->val);
+            if(q.front() -> left)
+                q.push(q.front() -> left);
+            if (q.front() -> right)
+                q.push(q.front() -> right);
+            q.pop();
+        }
+        return res;
+    }
+};
