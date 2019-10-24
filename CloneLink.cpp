264519f93e7f7325409fece8eda55848f8d37b61
Maicius <maicius@outlook.com>
2019-10-24 14:59:27 +0800

    复制复杂链表

---
+++ CloneLink.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+/**
+ * 题目描述
+输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，另一个特殊指针指向任意一个节点），返回结果为复制后复杂链表的head。
+（注意，输出结果中请不要返回参数中的节点引用，否则判题程序会直接返回空）
+ */ 
+struct RandomListNode {
+    int label;
+    struct RandomListNode *next, *random;
+    RandomListNode(int x) :
+            label(x), next(NULL), random(NULL) {
+    }
+};
+
+class Solution {
+public:
+    RandomListNode* Clone(RandomListNode* pHead)
+    {
+        if(!pHead) return NULL;
+        RandomListNode *h = pHead;
+        while (h) {
+            RandomListNode *node = new RandomListNode(h->label);
+            node -> next = h -> next;
+            h -> next = node;
+            h = node -> next;
+        }
+        h = pHead;
+        while (h) {
+            if (h -> random) {
+                h -> next -> random = h -> random -> next;
+            }
+            h = h -> next -> next;
+        }
+        //拆分
+        RandomListNode *pCloneHead = pHead->next;
+        RandomListNode *tmp;
+        h = pHead;
+        while(h -> next){
+            tmp = h -> next;
+            h -> next = tmp -> next;
+            h = tmp;
+        }
+        return pCloneHead;
+    }
+};
