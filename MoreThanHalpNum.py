9d9446c05b5478d44bd6bee77275bfa575c5304c
Maicius <maicius@outlook.com>
2019-10-27 15:42:43 +0800

    第三种解法，排序

--- MoreThanHalpNum.py
+++ MoreThanHalpNum.py
@@ -62,4 +62,33 @@ class Solution:
             if i == numbers[length // 2]:
                 count += 1
         return numbers[length // 2] if count > length / 2.0 else 0
+    
+
+    """对列表排序，计算下标为len/2(即中间位置)的数字数量即可"""
+    def MoreThanHalfNum_Solution3(self, numbers):
+        # write code here
+        # 对列表进行快排
+        left = 0
+        right = len(numbers) - 1
+        stack = [right, left]
+        while stack:
+            low = stack.pop()
+            high = stack.pop()
+            if low >= high:
+                continue
+            less = low - 1
+            mid = numbers[high]
+            for i in range(low, high):
+                if numbers[i] <= mid:
+                    less += 1
+                    numbers[less], numbers[i] = numbers[i], numbers[less]
+            numbers[less + 1], numbers[high] = numbers[high], numbers[less + 1]
+            stack.extend([high, less+2, less, low])
+        # 验证
+        count = 0
+        length = len(numbers) // 2
+        for i in numbers:
+            if i == numbers[length // 2]:
+                count += 1
+        return numbers[length // 2] if count > length / 2.0 else 0
             
