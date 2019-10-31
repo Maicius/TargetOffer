34e290c7eb2cf0ab9de8bdd0663a7a264511d53e
Maicius <maicius@outlook.com>
2019-10-31 13:35:12 +0800

    另外一种算法

--- FindNumbersWithSum.cpp
+++ FindNumbersWithSum.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-
+typedef vector<int> vi;
 class Solution {
 public:
     vector<int> FindNumbersWithSum(vector<int> array,int sum) {
@@ -36,13 +36,35 @@ public:
         }
         return res;
     }
-};
+
+/**
+数列满足递增，设两个头尾两个指针i和j，
+若ai + aj == sum，就是答案（相差越远乘积越小）
+若ai + aj > sum，aj肯定不是答案之一（前面已得出 i 前面的数已是不可能），j -= 1
+若ai + aj < sum，ai肯定不是答案之一（前面已得出 j 后面的数已是不可能），i += 1
+ */
+    vi FindNumbersWithSum2(const vi& a,int sum) {
+            vi res;
+            int n = a.size();
+            int i = 0, j = n - 1;
+            while(i < j){
+                if(a[i] + a[j] == sum){
+                    res.push_back(a[i]);
+                    res.push_back(a[j]);
+                    break;
+                }
+                while(i < j && a[i] + a[j] > sum) --j;
+                while(i < j && a[i] + a[j] < sum) ++i;
+            }
+            return res;
+        }
+    };
 
 int main() {
     int data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     vector<int> data1(data, data + 10);
     Solution *so = new Solution();
-    vector<int> res = so -> FindNumbersWithSum(data1, 10);
+    vector<int> res = so -> FindNumbersWithSum2(data1, 10);
     for (int i = 0; i < res.size(); i++) {
         cout << res[i];
     }
